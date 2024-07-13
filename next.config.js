3bd885db17c285c30e744086c02a7f0814c58102
y4mix77 <casaxanas@gmail.com>
2024-07-13 02:30:23 +0100

    initial commit

---
+++ next.config.js
@@ -0,0 +1,31 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/sign-in',
+        destination: '/api/auth/login',
+        permanent: true,
+      },
+      {
+        source: '/sign-up',
+        destination: '/api/auth/register',
+        permanent: true,
+      },
+    ]
+  },
+  images: {
+    domains: ['avatars.githubusercontent.com'],
+  },
+
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, webpack }
+  ) => {
+    config.resolve.alias.canvas = false
+    config.resolve.alias.encoding = false
+    return config
+  },
+}
+
+module.exports = nextConfig
